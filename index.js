ed89e79ec24f2950ef454ce4842d1a632226e020
David René <56747050+davidreneuw@users.noreply.github.com>
2024-03-20 04:11:53 +0000

    Location section

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function toggleNav() {
-    var x = document.getElementById("nav");
+  var x = document.getElementById("nav");
   if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
@@ -10,4 +10,19 @@ function toggleNav() {
 function closeNav() {
   var x = document.getElementById("nav");
   x.style.display = "none";
+}
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 43.3694038, lng: -80.3990211 },
+    zoom: 10,
+  });
+  var marker = new google.maps.Marker({
+    position: { lat: 43.3694038, lng: -80.3990211 },
+    title: "Whistlebear Golf Club",
+    icon: "https://kcweddingassets.blob.core.windows.net/assets/heart-solid.svg",
+  });
+  marker.setMap(map);
 }
